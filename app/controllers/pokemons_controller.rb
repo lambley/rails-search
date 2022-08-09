a84778490f5286113e892c695519ed98cf9cc531
Aaron Lambley <aaron.lambley@gmail.com>
2022-08-09 18:37:30 +0100

    basic search set up

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,5 +1,9 @@
 class PokemonsController < ApplicationController
   def index
-    @pokemons = Pokemon.all
+    if params[:query].present?
+      @pokemons = Pokemon.where("name ILIKE ?", "%#{params[:query]}%")
+    else
+      @pokemons = Pokemon.all
+    end
   end
 end
