6784f1e108d591e482a80c99200e12b80f0fbf7f
Aaron Lambley <aaron.lambley@gmail.com>
2022-08-15 12:16:27 +0100

    create refresh list stimulus controller. add update method

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import RefreshListController from "./refresh_list_controller"
+application.register("refresh-list", RefreshListController)
