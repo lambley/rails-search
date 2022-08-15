46e047dc34f06c6b77feaff57c54e3cdb0c9dc96
Aaron Lambley <aaron.lambley@gmail.com>
2022-08-15 12:17:19 +0100

    pokemon index search update to ajax

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,9 +1,15 @@
 class PokemonsController < ApplicationController
   def index
-    if params[:query].present?
-      @pokemons = Pokemon.search_by_name_and_number(params[:query])
-    else
-      @pokemons = Pokemon.all
+    # default to find all pokemons
+    @pokemons = Pokemon.all
+
+    # run search if query is present
+    @pokemons = @pokemons.search_by_name_and_number(params[:query]) if params[:query].present?
+
+    # set action to respond to text/plain and render partial
+    respond_to do |format|
+      format.html
+      format.text { render partial: 'pokemons/list', formats: [:html], locals: { pokemons: @pokemons } }
     end
   end
 end
