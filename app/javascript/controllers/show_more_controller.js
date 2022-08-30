68ae525c23d30d312d34c8d99acdc578bc86592e
Aaron Lambley <aaron.lambley@gmail.com>
2022-08-30 16:08:06 +0100

    update card width for mobile; remove connect msg

--- app/javascript/controllers/show_more_controller.js
+++ app/javascript/controllers/show_more_controller.js
@@ -4,7 +4,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["show", "entry", "close"]
   connect() {
-    console.log("show more connected");
   }
   displayEntry(event) {
     console.log(event.path[2].innerText);
